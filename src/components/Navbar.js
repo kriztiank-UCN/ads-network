917ee8d1fc0324eb354aad62fd5bc7a0b71e735d
Kristian <10221425@ucn.dk>
2024-04-02 15:27:33 +0200

    Check authentication state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { AuthContext } from '../context/auth'
 
 const Navbar = () => {
+  // destructure the user object from the AuthContext
+  const { user } = useContext(AuthContext)
+
   return (
-    <nav className="navbar navbar-expand-md bg-light navbar-light sticky-top shadow-sm">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
+    <nav className='navbar navbar-expand-md bg-light navbar-light sticky-top shadow-sm'>
+      <div className='container'>
+        <Link className='navbar-brand' to='/'>
           Ads Network
         </Link>
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link" to="/auth/register">
-                Register
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/auth/login">
-                Login
-              </Link>
-            </li>
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
+            {user ? (
+              // if the user is logged in, display a log out button
+              <>
+                <button className='btn btn-danger btn-sm'>Log out</button>
+              </>
+            ) : (
+              // if the user is not logged in, display a register and login link
+              <>
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/auth/register'>
+                    Register
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/auth/login'>
+                    Login
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
---
+++ src/context/auth.js
@@ -0,0 +1,29 @@
+import { createContext, useState, useEffect } from 'react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../firebaseConfig'
+import Loading from '../components/Loading'
+// Create a new context
+export const AuthContext = createContext()
+// Create a new provider
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  // Track the user's login status
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      setUser(user)
+      // Set loading to false after the user's login status has been determined
+      setLoading(false)
+    })
+  }, [])
+
+  if (loading) {
+    // if the user's login status is still being determined, display a loading spinner
+    return <Loading />
+  }
+  // Pass the user's login status to the value prop of the provider
+  return <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+}
+
+export default AuthProvider
