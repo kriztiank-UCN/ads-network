b7937e1ec8e9482eb13347f4eb52a60eb3872411
Kristian <10221425@ucn.dk>
2024-04-02 14:38:13 +0200

    Register new user in firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,8 +15,14 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
---
+++ src/pages/auth/Register.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react'
+import { auth, db } from '../../firebaseConfig'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { setDoc, doc, Timestamp } from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom'
+
+const Register = () => {
+  // create state variables and set default values
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    error: '',
+    loading: false,
+  })
+  // useNavigate to redirect to home page
+  const navigate = useNavigate()
+  // destructure default values from the useState getter
+  const { name, email, password, confirmPassword, error, loading } = values
+
+  // { ...values, [e.target.name]: e.target.value } : This is an object that is passed to setValues. The ...values part is using the spread operator to create a new object with the same properties as the current values state.
+  const handleChange = e => setValues({ ...values, [e.target.name]: e.target.value })
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    // validate fields
+    if (!name || !email || !password || !confirmPassword) {
+      setValues({ ...values, error: 'All fields are required' })
+      return
+    }
+    if (password !== confirmPassword) {
+      setValues({ ...values, error: 'Password must match' })
+      return
+    }
+    // clear error message and set loading to true
+    setValues({ ...values, error: '', loading: true })
+
+    try {
+      // register user
+      const result = await createUserWithEmailAndPassword(auth, email, password)
+      // create user document in firestore
+      await setDoc(doc(db, 'users', result.user.uid), {
+        uid: result.user.uid,
+        name,
+        email,
+        createdAt: Timestamp.fromDate(new Date()),
+        isOnline: true,
+      })
+      // clear state, setValues back to default
+      setValues({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        error: '',
+        loading: false,
+      })
+      // redirect to home page
+      navigate('/')
+      // error handling ...values will return an object with the current values state, the error property is set to the error message.
+    } catch (error) {
+      setValues({ ...values, error: error.message, loading: false })
+    }
+  }
+  return (
+    <form className='shadow rounded p-3 mt-5 form' onSubmit={handleSubmit}>
+      <h3 className='text-center mb-3'>Create An Account</h3>
+      <div className='mb-3'>
+        <label htmlFor='name' className='form-label'>
+          Name
+        </label>
+        <input
+          type='text'
+          className='form-control'
+          name='name'
+          value={name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='email' className='form-label'>
+          Email
+        </label>
+        <input
+          type='email'
+          className='form-control'
+          name='email'
+          value={email}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='password' className='form-label'>
+          Password
+        </label>
+        <input
+          type='password'
+          className='form-control'
+          name='password'
+          value={password}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='confirmPassword' className='form-label'>
+          Confirm Password
+        </label>
+        <input
+          type='password'
+          className='form-control'
+          name='confirmPassword'
+          value={confirmPassword}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='text-center mb-3'>
+        <button className='btn btn-secondary btn-sm' disabled={loading}>
+          Register
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default Register
