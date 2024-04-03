1b247643c39534a3557b979721891f0844a88a0d
Kristian <10221425@ucn.dk>
2024-04-03 10:48:23 +0200

    Login user

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { auth, db } from '../../firebaseConfig'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { doc, updateDoc } from 'firebase/firestore'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  // create state variables and set default values
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    error: '',
+    loading: false,
+  })
+  // useNavigate to redirect to home page
+  const navigate = useNavigate()
+  // destructure default values from the useState getter
+  const { email, password, error, loading } = values
+  // { ...values, [e.target.name]: e.target.value } : This is an object that is passed to setValues. The ...values part is using the spread operator to create a new object with the same properties as the current values state.
+  const handleChange = e => setValues({ ...values, [e.target.name]: e.target.value })
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    // validate fields
+    if (!email || !password) {
+      setValues({ ...values, error: 'All fields are required' })
+      return
+    }
+    // clear error message and set loading to true
+    setValues({ ...values, error: '', loading: true })
+
+    try {
+      // register user
+      const result = await signInWithEmailAndPassword(auth, email, password)
+      // update user document in firestore
+      await updateDoc(doc(db, 'users', result.user.uid), {
+        isOnline: true,
+      })
+      // clear state, setValues back to default
+      setValues({
+        email: '',
+        password: '',
+        error: '',
+        loading: false,
+      })
+      // redirect to home page
+      navigate('/')
+      // error handling ...values will return an object with the current values state, the error property is set to the error message.
+    } catch (error) {
+      setValues({ ...values, error: error.message, loading: false })
+    }
+  }
   return (
-    <div>Login</div>
+    <form className='shadow rounded p-3 mt-5 form' onSubmit={handleSubmit}>
+      <h3 className='text-center mb-3'>Log Into Your Account</h3>
+      <div className='mb-3'>
+        <label htmlFor='email' className='form-label'>
+          Email
+        </label>
+        <input
+          type='email'
+          className='form-control'
+          name='email'
+          value={email}
+          onChange={handleChange}
+        />
+      </div>
+      <div className='mb-3'>
+        <label htmlFor='password' className='form-label'>
+          Password
+        </label>
+        <input
+          type='password'
+          className='form-control'
+          name='password'
+          value={password}
+          onChange={handleChange}
+        />
+      </div>
+      {error ? <p className='text-center text-danger'>{error}</p> : null}
+      <div className='text-center mb-3'>
+        <button className='btn btn-secondary btn-sm' disabled={loading}>
+          Login
+        </button>
+      </div>
+    </form>
   )
 }
 
