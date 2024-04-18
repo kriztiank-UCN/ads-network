dd5858e393b03da7c3ad8d2d7112985d402f425c
Kristian <10221425@ucn.dk>
2024-04-18 10:41:32 +0200

    Delete an ad

--- src/pages/Ad.js
+++ src/pages/Ad.js
@@ -1,117 +1,125 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { doc, getDoc } from "firebase/firestore";
-import { auth, db } from "../firebaseConfig";
-import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-import { FaTrashAlt } from "react-icons/fa";
-import Moment from "react-moment";
+import React, { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { deleteDoc, doc, getDoc } from 'firebase/firestore'
+import { auth, db, storage } from '../firebaseConfig'
+import { ref, deleteObject } from 'firebase/storage'
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
+import { FaTrashAlt } from 'react-icons/fa'
+import Moment from 'react-moment'
 // import custom hook
-import useSnapshot from "../utils/useSnapshot";
-import { toggleFavorite } from "../utils/fav";
+import useSnapshot from '../utils/useSnapshot'
+import { toggleFavorite } from '../utils/fav'
 
 const Ad = () => {
-  const { id } = useParams();
-  const [ad, setAd] = useState();
-  const [idx, setIdx] = useState(0);
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const [ad, setAd] = useState()
+  const [idx, setIdx] = useState(0)
   // initialize custom hook
-  const { val } = useSnapshot("favorites", id);
+  const { val } = useSnapshot('favorites', id)
 
   const getAd = async () => {
-    const docRef = doc(db, "ads", id);
-    const docSnap = await getDoc(docRef);
+    const docRef = doc(db, 'ads', id)
+    const docSnap = await getDoc(docRef)
     if (docSnap.exists()) {
-      setAd(docSnap.data());
+      setAd(docSnap.data())
     }
-  };
+  }
 
   useEffect(() => {
-    getAd();
-  }, []);
+    getAd()
+  }, [])
   // console.log(ad);
-  
+
+  const deleteAd = async () => {
+    const confirm = window.confirm(`Delete ${ad.title}?`)
+    if (confirm) {
+      // delete images
+      for (const image of ad.images) {
+        const imgRef = ref(storage, image.path)
+        await deleteObject(imgRef)
+      }
+      // delete fav doc from firestore
+      await deleteDoc(doc(db, 'favorites', id))
+      // delete ad doc from firestore
+      await deleteDoc(doc(db, 'ads', id))
+      // navigate to seller profile
+      navigate(`/profile/${auth.currentUser.uid}`)
+    }
+  }
+
   return ad ? (
-    <div className="mt-5 container">
-      <div className="row">
-        <div id="carouselExample" className="carousel slide col-md-8">
-          <div className="carousel-inner">
+    <div className='mt-5 container'>
+      <div className='row'>
+        <div id='carouselExample' className='carousel slide col-md-8'>
+          <div className='carousel-inner'>
             {ad.images.map((image, i) => (
-              <div
-                className={`carousel-item ${idx === i ? "active" : ""}`}
-                key={i}
-              >
-                <img src={image.url} className="d-block w-100" alt={ad.title} />
+              <div className={`carousel-item ${idx === i ? 'active' : ''}`} key={i}>
+                <img src={image.url} className='d-block w-100' alt={ad.title} />
 
                 <button
-                  className="carousel-control-prev"
-                  type="button"
-                  data-bs-target="#carouselExample"
-                  data-bs-slide="prev"
+                  className='carousel-control-prev'
+                  type='button'
+                  data-bs-target='#carouselExample'
+                  data-bs-slide='prev'
                   onClick={() => setIdx(i)}
                 >
-                  <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Previous</span>
+                  <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+                  <span className='visually-hidden'>Previous</span>
                 </button>
                 <button
-                  className="carousel-control-next"
-                  type="button"
-                  data-bs-target="#carouselExample"
-                  data-bs-slide="next"
+                  className='carousel-control-next'
+                  type='button'
+                  data-bs-target='#carouselExample'
+                  data-bs-slide='next'
                   onClick={() => setIdx(i)}
                 >
-                  <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Next</span>
+                  <span className='carousel-control-next-icon' aria-hidden='true'></span>
+                  <span className='visually-hidden'>Next</span>
                 </button>
               </div>
             ))}
           </div>
         </div>
-        <div className="col-md-4">
-          <div className="card">
-            <div className="card-body">
-              <div className="d-flex justify-content-between align-items-center">
-                <h5 className="card-title">
-                  PKR. {Number(ad.price).toLocaleString()}
-                </h5>
+        <div className='col-md-4'>
+          <div className='card'>
+            <div className='card-body'>
+              <div className='d-flex justify-content-between align-items-center'>
+                <h5 className='card-title'>PKR. {Number(ad.price).toLocaleString()}</h5>
                 {val?.users?.includes(auth.currentUser?.uid) ? (
                   <AiFillHeart
                     size={30}
                     onClick={() => toggleFavorite(val.users, id)}
-                    className="text-danger"
+                    className='text-danger'
                   />
                 ) : (
                   <AiOutlineHeart
                     size={30}
                     onClick={() => toggleFavorite(val.users, id)}
-                    className="text-danger"
+                    className='text-danger'
                   />
                 )}
               </div>
-              <h6 className="card-subtitle mb-2">{ad.title}</h6>
-              <div className="d-flex justify-content-between">
-                <p className="card-text">
-                  {ad.location} -{" "}
+              <h6 className='card-subtitle mb-2'>{ad.title}</h6>
+              <div className='d-flex justify-content-between'>
+                <p className='card-text'>
+                  {ad.location} -{' '}
                   <small>
                     <Moment fromNow>{ad.publishedAt.toDate()}</Moment>
                   </small>
                 </p>
-                <FaTrashAlt size={20} className="text-danger" />
+                <FaTrashAlt size={20} className='text-danger' onClick={deleteAd} />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="mt-5">
+      <div className='mt-5'>
         <h3>Description</h3>
         <p>{ad.description}</p>
       </div>
     </div>
-  ) : null;
-};
+  ) : null
+}
 
-export default Ad;
+export default Ad
